7f4688810a4f021ceaa2d3fbd7314a31e621b4ba
Ben Soyka <hello@bsoyka.me>
2025-12-19 18:46:53 -0700

    style(fish): clean up config

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,23 +1,10 @@
-set PATH $PATH /Users/bsoyka/.local/bin
+fish_add_path ~/.local/bin
 
 source ~/.config/fish/secrets.fish
 
+# ----- SET UP TOOLS -----
 direnv hook fish | source
-
 zoxide init fish | source
-
 thefuck --alias | source
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
-
-abbr --add cdgh cd ~/Documents/GitHub
-
-# BEGIN opam configuration
-# This is useful if you're using opam as it adds:
-#   - the correct directories to the PATH
-#   - auto-completion for the opam binary
-# This section can be safely removed at any time if needed.
 test -r '/Users/bsoyka/.opam/opam-init/init.fish' && source '/Users/bsoyka/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
-# END opam configuration
