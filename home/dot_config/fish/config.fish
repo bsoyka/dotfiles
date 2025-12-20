33d53a6697070cdf4fcbe44489bdf48b7a0afc15
Ben Soyka <hello@bsoyka.me>
2025-12-20 00:26:12 -0700

    feat(fish): add iterm2 integration

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -8,3 +8,4 @@ zoxide init fish | source
 thefuck --alias | source
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 test -r '/Users/bsoyka/.opam/opam-init/init.fish' && source '/Users/bsoyka/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
